be6a2a2d3d58e014b7815370b3053d3af3dc658f
EntropyYue <xiaopa233@qq.com>
2025-09-18 03:05:47 +0800

    Revert "refac: styling"
    This reverts commit 042191372a52123fbe40c0d35fac946fc8a6c964.

--- src/lib/components/chat/Messages/Citations.svelte
+++ src/lib/components/chat/Messages/Citations.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
-	import CitationModal from './Citations/CitationModal.svelte';
+	import CitationsModal from '$lib/components/chat/Messages/Citations/CitationsModal.svelte';
 
 	const i18n = getContext('i18n');
 
@@ -12,17 +12,15 @@
 	let showRelevance = true;
 
 	let citationModal = null;
-
-	let showCitations = false;
 	let showCitationModal = false;
-
 	let selectedCitation: any = null;
+	let isCollapsibleOpen = false;
 
 	export const showSourceModal = (sourceIdx) => {
 		if (citations[sourceIdx]) {
 			console.log('Showing citation modal for:', citations[sourceIdx]);
-			selectedCitation = citations[sourceIdx];
-			showCitationModal = true;
+			citationModal?.showCitation(citations[sourceIdx]);
+			// showCitationModal = true;
 		}
 	};
 
@@ -96,19 +94,13 @@
 		showRelevance = calculateShowRelevance(citations);
 		showPercentage = shouldShowPercentage(citations);
 	}
-
-	const decodeString = (str: string) => {
-		try {
-			return decodeURIComponent(str);
-		} catch (e) {
-			return str;
-		}
-	};
 </script>
 
-<CitationModal
+<CitationsModal
+	bind:this={citationModal}
 	bind:show={showCitationModal}
-	citation={selectedCitation}
+	{id}
+	{citations}
 	{showPercentage}
 	{showRelevance}
 />
@@ -119,7 +111,7 @@
 		<button
 			class="text-xs font-medium text-gray-600 dark:text-gray-300 px-3.5 h-8 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800 transition flex items-center gap-1 border border-gray-50 dark:border-gray-850"
 			on:click={() => {
-				showCitations = !showCitations;
+				showCitationModal = true;
 			}}
 		>
 			{#if urlCitations.length > 0}
@@ -143,31 +135,5 @@
 				{/if}
 			</div>
 		</button>
-	</div>
-{/if}
-
-{#if showCitations}
-	<div class="py-1.5">
-		<div class="text-xs gap-2 flex flex-col">
-			{#each citations as citation, idx}
-				<button
-					id={`source-${id}-${idx + 1}`}
-					class="no-toggle outline-hidden flex dark:text-gray-300 bg-transparent text-gray-600 rounded-xl gap-1.5 items-center"
-					on:click={() => {
-						showCitationModal = true;
-						selectedCitation = citation;
-					}}
-				>
-					<div class=" font-medium bg-gray-50 dark:bg-gray-850 rounded-md px-1">
-						{idx + 1}
-					</div>
-					<div
-						class="flex-1 truncate hover:text-black dark:text-white/60 dark:hover:text-white transition text-left"
-					>
-						{decodeString(citation.source.name)}
-					</div>
-				</button>
-			{/each}
-		</div>
 	</div>
 {/if}
